b440f159085778effc4d12a958e49aa10d0942ee
student <colinchomas@msn.com>
2026-02-05 18:01:16 -0500

    bitte

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require( 'express' ),
+      router = express.Router(),
+      { Client } = require('pg');
+      
+// establish database connection
+const client = new Client({
+  host: 'localhost',
+  database: 'siteinfo',
+  user: 'student',
+  password: 'student',
+  port: 5432,
+});
+
+client.connect();
+
+// GET home page
+router.get('/', async ( req, res ) => { 
+      
+  try {
+
+    const queryResult = await client.query('SELECT * FROM defaults WHERE id=1');
+
+    // Respond with the queried data
+    res.render( `index`, { title: queryResult.rows[0].sitename });
+  }
+  catch ( error ) {
+    console.error( error );
+    res.status( 500 ).send( 'Internal Server Error' );
+  }
+    
+});
+
+module.exports = router;
+
+
